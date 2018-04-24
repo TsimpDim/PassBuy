7aa9866665f6cefbb6c06ed3726c177903a6c691
TsimpDim <dimitristsimpitas@yahoo.gr>
2018-04-24 13:03:19 +0300

    Added /prods/id endpoint

--- back-end/PassBuy_API/routes.js
+++ back-end/PassBuy_API/routes.js
@@ -17,14 +17,39 @@ router.get('/categories', function(req, res) {
 
         res.send(result);
     });
-
 });
+
 router.get('/categories/:id', function(req, res) {
     db.query("SELECT * FROM categories WHERE category_id = ?",[req.params.id],
     function(err, result, fields){
         if(err) throw err;
 
         res.send(result);
+    });
+});
+
+router.get('/products/:id', function(req, res) {
+
+    db.query("SELECT * FROM products WHERE product_id = ?",[req.params.id], // Get product info
+    function(err, result, fields){
+
+        if(err) throw err;
+
+        let response = result[0];
+
+        // When the previous query ends, start a new one for the prices
+        db.query("SELECT store_id, price FROM product_prices WHERE product_id = ?",[req.params.id], // Get price info
+        function(err, result, fields){
+            if(err){
+                console.log(err);
+                response.prices = {"error" : "No prices found"};
+            }else{
+                response.prices = result;
+            }
+
+            res.send(response); // And when the new one finishes as well, send the response
+        });
+    
     });
 
 });
