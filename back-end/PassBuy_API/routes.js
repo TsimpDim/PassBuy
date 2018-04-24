9ef40d679df2aea2cd35d1c50b938b50e61cd719
TsimpDim <dimitristsimpitas@yahoo.gr>
2018-04-24 12:06:46 +0300

    Added /prices/id endpoint

--- back-end/PassBuy_API/routes.js
+++ back-end/PassBuy_API/routes.js
@@ -17,8 +17,8 @@ router.get('/categories', function(req, res) {
 
         res.send(result);
     });
-
 });
+
 router.get('/categories/:id', function(req, res) {
     db.query("SELECT * FROM categories WHERE category_id = ?",[req.params.id],
     function(err, result, fields){
@@ -26,7 +26,26 @@ router.get('/categories/:id', function(req, res) {
 
         res.send(result);
     });
+});
 
+router.get('/prices/:pr_id', function(req, res) {
+    db.query("SELECT * FROM product_prices WHERE product_id = ?",[req.params.pr_id],
+    function(err, result, fields){
+        if(err) throw err;
+        
+        // Set up response object
+        let response = {"product_id" : req.params.pr_id}; // Default value is the product_id itself
+        response.prices = []; // Add prices array
+
+        result.forEach(el => {
+            response.prices.push({
+                "store_id" : el.store_id,
+                "price" : el.price
+            });
+        });
+
+        res.send(response);
+    });
 });
 
 module.exports = router;
