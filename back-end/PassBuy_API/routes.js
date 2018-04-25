134646b23ec11358a6fd1b414c8b97f270b762ce
iamaldi <dialcode@outlook.com>
2018-04-25 18:31:39 +0300

    Revert "Change db.js filename"
    This reverts commit e39b63bcbf5f6c6c0991b3b32a74efdf3900de61.

--- back-end/PassBuy_API/routes.js
+++ back-end/PassBuy_API/routes.js
@@ -1,8 +1,8 @@
 let express = require('express');
 let router = express.Router();
-let db = require('./db');
+let db = require('./connection');
 
-
+  
 router.get('/stores', function(req, res) {
     db.query("SELECT * FROM stores", function(err, result, fields){
         if(err) throw err;
