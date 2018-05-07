92652490c0a8f8e27b3f5976563cffa4ebf1435b
DioExtreme <diodotexe@gmail.com>
2018-05-07 13:43:41 +0300

    Implement more-info layout
    Relies on a RecyclerView with a LinearLayoutManager to show all
    the shops. Currently, string and ItemCount values are hardcoded for
    demonstration purposes but will be changed later on.

---
+++ front-end/PassBuy/app/src/main/java/thedreamteam/passbuy/MoreInfoActivity.java
@@ -0,0 +1,37 @@
+package thedreamteam.passbuy;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+public class MoreInfoActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.more_info);
+        mRecyclerView = findViewById(R.id.rv);
+
+        // Layout size remains fixed, improve performance
+        mRecyclerView.setHasFixedSize(true);
+
+        // Use a linear layout manager
+        // A vertically-scrollable collection of views
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // Use an adapter to feed data into the RecyclerView
+        mAdapter = new MoreInfoAdapter();
+        mRecyclerView.setAdapter(mAdapter);
+
+        // Draw line divider
+        DividerItemDecoration lineDivider = new DividerItemDecoration(this, DividerItemDecoration.VERTICAL);
+        mRecyclerView.addItemDecoration(lineDivider);
+    }
+}
---
+++ front-end/PassBuy/app/src/main/java/thedreamteam/passbuy/MoreInfoAdapter.java
@@ -0,0 +1,43 @@
+package thedreamteam.passbuy;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class MoreInfoAdapter extends RecyclerView.Adapter<MoreInfoAdapter.ViewHolder> {
+    
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.more_info_recycler, parent, false);
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, final int position) {
+        // todo
+    }
+
+    @Override
+    public int getItemCount() {
+        // Decides how many shops will appear
+        // Hardcoded for demo, should be replaced with a Length function
+        return 10;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView name;
+        public TextView area;
+        public TextView total;
+
+        // Speeds up rendering by reducing findViewById calls
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            name = itemView.findViewById(R.id.name);
+            area = itemView.findViewById(R.id.area);
+            total = itemView.findViewById(R.id.total);
+        }
+    }
+}
