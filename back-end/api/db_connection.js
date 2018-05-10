b4a54c09b091d97f1637b52381a543cc3b5c9ca8
iamaldi <dialcode@outlook.com>
2018-05-10 18:03:05 +0300

    Add Database connection pooling

--- back-end/api/db_connection.js
+++ back-end/api/db_connection.js
@@ -1,21 +1,16 @@
 let mysql = require('mysql');
 let db_creds = require('./db_creds.json'); // DB config file
-let db;
+let pool  = mysql.createPool(db_creds); // Create DB connection pool
 
-
-
-function connectDatabase(){
-    if(!db){
-        db = mysql.createConnection(db_creds);
-
-        db.connect(function(err){ // Connect to DB
-            if(err) throw err;
-
-            console.log("Database connection successful.");
-        });
+// Test DB connection
+pool.getConnection((err) => {
+    if(err){
+      // Display only an error message
+      console.log("Error: Could not connect to database");
+  }
+    else {
+      console.log("Database connection successful");
     }
+});
 
-    return db;
-}
-
-module.exports = connectDatabase();
+module.exports = pool;
