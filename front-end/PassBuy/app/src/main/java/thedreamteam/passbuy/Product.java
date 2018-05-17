c6dc18cba9b7ef195d07451ab2146e7440c5f77c
DioExtreme <diodotexe@gmail.com>
2018-05-17 22:03:17 +0300

    Implement Product class based on API data

---
+++ front-end/PassBuy/app/src/main/java/thedreamteam/passbuy/Product.java
@@ -0,0 +1,78 @@
+package thedreamteam.passbuy;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Product {
+    @SerializedName("name")
+    @Expose
+    private String name;
+    @SerializedName("description")
+    @Expose
+    private String description;
+    @SerializedName("image_url")
+    @Expose
+    private String imageUrl;
+    @SerializedName("category")
+    @Expose
+    private Integer category;
+    @SerializedName("product_id")
+    @Expose
+    private Integer productId;
+    @SerializedName("prices")
+    @Expose
+    private List<Price> prices = new ArrayList<>();
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public Integer getCategory() {
+        return category;
+    }
+
+    public void setCategory(Integer category) {
+        this.category = category;
+    }
+
+    public Integer getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Integer productId) {
+        this.productId = productId;
+    }
+
+    public List<Price> getPrices() {
+        return prices;
+    }
+
+    public void setPrices(List<Price> prices) {
+        this.prices = prices;
+    }
+}
+
+
