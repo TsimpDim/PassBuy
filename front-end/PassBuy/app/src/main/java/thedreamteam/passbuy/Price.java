7eb3b21c08768663f433ae33d085378100e3f3c2
DioExtreme <diodotexe@gmail.com>
2018-05-17 22:03:33 +0300

    Implement Price class based on API data

---
+++ front-end/PassBuy/app/src/main/java/thedreamteam/passbuy/Price.java
@@ -0,0 +1,29 @@
+package thedreamteam.passbuy;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Price {
+    @SerializedName("store_id")
+    @Expose
+    private Integer storeId;
+    @SerializedName("price")
+    @Expose
+    private Double price;
+
+    public Integer getStoreId() {
+        return storeId;
+    }
+
+    public void setStoreId(Integer storeId) {
+        this.storeId = storeId;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+}
