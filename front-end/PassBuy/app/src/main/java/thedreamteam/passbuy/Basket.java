1fed0b406cac00f722b67a85a62f44ca7e55b7ac
alvincein <alvincein@gmail.com>
2018-05-25 17:13:27 +0300

    home-screen-classes

---
+++ front-end/PassBuy/app/src/main/java/thedreamteam/passbuy/Basket.java
@@ -0,0 +1,39 @@
+package thedreamteam.passbuy;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Basket implements Serializable {
+
+    private List<Product> products ;
+    private ArrayList<Integer> quantities;
+    private List<Price> total_prices;
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+
+    public ArrayList<Integer> getQuantities() {
+        return quantities;
+    }
+
+    public void setQuantities(ArrayList<Integer> quantities) {
+        this.quantities = quantities;
+    }
+
+    public List<Price> getTotalprices() {
+        return total_prices;
+    }
+
+    public void setTotalprices(List<Price> total_prices) {
+        this.total_prices = total_prices;
+    }
+}
--- front-end/PassBuy/app/src/main/java/thedreamteam/passbuy/HomeScreenBasketAdapter.java
+++ front-end/PassBuy/app/src/main/java/thedreamteam/passbuy/HomeScreenBasketAdapter.java
@@ -1,7 +1,6 @@
 package thedreamteam.passbuy;
 
 import android.content.Context;
-import android.support.annotation.NonNull;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -10,20 +9,22 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.List;
 
-public class HomeScreenBasketAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
+public class HomeScreenBasketAdapter extends RecyclerView.Adapter<HomeScreenBasketAdapter.ViewHolder>{
 
     private static final String TAG = "RecyclerViewAdapter";
 
-    private ArrayList<String> mItemNames = new ArrayList<>();
-    private ArrayList<String> mItemQ = new ArrayList<>();
+    private List<Product> Products;
+    private ArrayList<Integer> Quantities;
     private Context mContext;
 
 
-    public HomeScreenBasketAdapter( Context mContext, ArrayList<String> mItemNames, ArrayList<String> mItemQ) {
-        this.mItemNames = mItemNames;
-        this.mItemQ = mItemQ;
+    public HomeScreenBasketAdapter(Context mContext, List<Product> products, ArrayList<Integer> quantities) {
+        this.Products = products;
+        this.Quantities = quantities;
         this.mContext = mContext;
     }
 
@@ -35,14 +36,22 @@ public class HomeScreenBasketAdapter extends RecyclerView.Adapter<RecyclerView.V
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, final int position) {
-        //todo
+    public void onBindViewHolder(ViewHolder holder, int position) {
+
+        holder.item_name.setText(Products.get(position).getName());
+        holder.item_q.setText(Quantities.get(position).toString());
     }
 
     @Override
     public int getItemCount() {
-        return 10;
+        return Products.size();
     }
+
+    public void replaceList(List<Product> newList) {
+        Products.clear();
+        Products.addAll(newList);
+    }
+
 
     public class ViewHolder extends RecyclerView.ViewHolder{
 
@@ -54,9 +63,9 @@ public class HomeScreenBasketAdapter extends RecyclerView.Adapter<RecyclerView.V
         public ViewHolder(View itemView) {
             super(itemView);
             item_name = itemView.findViewById(R.id.item_name);
+            item_name.setSelected(true);
             item_q = itemView.findViewById(R.id.quantity_text);
             parent_layout = itemView.findViewById(R.id.parent_layout);
         }
     }
 }
-
