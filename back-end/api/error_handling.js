0aa3093d13949e0aca1fb14bdf9610d5371dabbc
TsimpDim <dimitristsimpitas@yahoo.gr>
2018-06-08 09:48:41 +0300

    Fixed error handler declaration

--- back-end/api/error_handling.js
+++ back-end/api/error_handling.js
@@ -1,15 +1,15 @@
-// JSON object to store the error message response
+// Error object
 let error_resp = {
-  // Default error message
   status : 'error',
   err_msg : 'An error has been encountered'
 };
 
-// Error Handler function to return a case-specific error message as JSON object
-handler = (error) => {
-  // Update the default error message
+let handler = (error) => {
+
+  // Update error message
   error_resp.err_msg = error;
   return error_resp;
+  
 }
 
 module.exports = handler;
