62199d2f047fb46701a3747d350873eaf4e98714
DioExtreme <diodotexe@gmail.com>
2018-06-15 11:44:37 +0300

    Refactor BasketInfoAdapter class

--- front-end/PassBuy/app/src/main/java/thedreamteam/passbuy/BasketInfoAdapter.java
+++ front-end/PassBuy/app/src/main/java/thedreamteam/passbuy/BasketInfoAdapter.java
@@ -1,7 +1,6 @@
 package thedreamteam.passbuy;
 
 import android.content.Context;
-import android.support.annotation.NonNull;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -9,20 +8,17 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import java.util.List;
 
-import java.util.ArrayList;
+public class BasketInfoAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
-public class BasketInfoAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
-
-    private static final String TAG = "RecyclerViewAdapter";
-
-    private ArrayList<String> mItemNames = new ArrayList<>();
-    private ArrayList<String> mItemPrices = new ArrayList<>();
-    private ArrayList<String> mItemQ = new ArrayList<>();
+    private List<String> mItemNames;
+    private List<String> mItemPrices;
+    private List<String> mItemQ;
     private Context mContext;
 
 
-    public BasketInfoAdapter( Context mContext, ArrayList<String> mItemNames, ArrayList<String> mItemQ, ArrayList<String> mItemPrices) {
+    public BasketInfoAdapter(Context mContext, List<String> mItemNames, List<String> mItemQ, List<String> mItemPrices) {
         this.mItemNames = mItemNames;
         this.mItemPrices = mItemPrices;
         this.mItemQ = mItemQ;
@@ -31,13 +27,12 @@ public class BasketInfoAdapter extends RecyclerView.Adapter<RecyclerView.ViewHol
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.basket_info_recycler,parent,false);
-        ViewHolder holder = new ViewHolder(view);
-        return holder;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.basket_info_recycler, parent, false);
+        return new ViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, final int position) {
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, final int position) {
         //todo
     }
 
@@ -46,9 +41,7 @@ public class BasketInfoAdapter extends RecyclerView.Adapter<RecyclerView.ViewHol
         return 10;
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder{
-
-
+    public class ViewHolder extends RecyclerView.ViewHolder {
         TextView item_price;
         TextView item_name;
         TextView item_q;
