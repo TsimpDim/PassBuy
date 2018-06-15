83dfe794b05b90698bb9fae4b59d4f0d9fa0f4b8
DioExtreme <diodotexe@gmail.com>
2018-06-15 11:57:53 +0300

    Refactor PopupQuantityDialog class

--- front-end/PassBuy/app/src/main/java/thedreamteam/passbuy/PopupQuantityDialog.java
+++ front-end/PassBuy/app/src/main/java/thedreamteam/passbuy/PopupQuantityDialog.java
@@ -1,10 +1,10 @@
 package thedreamteam.passbuy;
 
+import android.app.DialogFragment;
 import android.content.Context;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
-import android.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,18 +20,9 @@ import com.squareup.picasso.Picasso;
 public class PopupQuantityDialog extends DialogFragment {
 
     private int quantity = 0;
-    private Product product;
-    private Basket basket;
     private DialogListener dialogListener;
 
-    private TextView item_name;
-    private TextView description;
     private TextView count;
-    private ImageButton plus;
-    private ImageButton minus;
-    private ImageView image;
-    private Button cancel;
-    private Button add;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         //Create view
@@ -41,96 +32,58 @@ public class PopupQuantityDialog extends DialogFragment {
         this.getDialog().getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
 
 
-
-        basket = (Basket) getArguments().getSerializable("basket");
-        product = (Product) getArguments().getSerializable("product");
+        Basket basket = (Basket) getArguments().getSerializable("basket");
         Product product = (Product) getArguments().getSerializable("product");
 
 
-        item_name  = (TextView) view.findViewById(R.id.item_name);
-        description = (TextView) view.findViewById(R.id.prodDesc);
-        count = (TextView) view.findViewById(R.id.count);
-        plus = (ImageButton) view.findViewById(R.id.plus);
-        minus = (ImageButton) view.findViewById(R.id.minusButton);
-        image = (ImageView) view.findViewById(R.id.prodImage);
-        cancel = (Button) view.findViewById(R.id.cancel);
-        add = (Button) view.findViewById(R.id.add);
-
-        item_name.setText(product.getName());
-        description.setText(product.getDescription());
+        count = view.findViewById(R.id.count);
         count.setText(String.valueOf(quantity));
 
-        if(basket.getProducts().contains(product)){
+        TextView itemName = view.findViewById(R.id.item_name);
+        TextView description = view.findViewById(R.id.prodDesc);
+        ImageButton plus = view.findViewById(R.id.plus);
+        ImageButton minus = view.findViewById(R.id.minusButton);
+        ImageView image = view.findViewById(R.id.prodImage);
+        Button cancel = view.findViewById(R.id.cancel);
+        Button add = view.findViewById(R.id.add);
+
+        itemName.setText(product.getName());
+        description.setText(product.getDescription());
+
+        if (basket.getProducts().contains(product)) {
             quantity = basket.getQuantities().get(basket.getProducts().indexOf(product));
             count.setText(String.valueOf(basket.getQuantities().get(basket.getProducts().indexOf(product))));
         }
 
+        plus.setOnClickListener(v -> {
+            quantity++;
+            count.setText(String.valueOf(quantity));
+        });
 
-        //Better Way to do the thing above.
-        /*for (Product prod : basket.getProducts()){
-            if(prod.getName().equals(product.getName())){
-                quantity = basket.getQuantities().get(basket.getProducts().indexOf(product));
-                count.setText(String.valueOf(basket.getQuantities().get(basket.getProducts().indexOf(product))));
-            }
-        }
-        */
-
-
-        plus.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                quantity ++;
+        minus.setOnClickListener(v -> {
+            if (quantity != 0) {
+                quantity--;
                 count.setText(String.valueOf(quantity));
             }
         });
 
-        minus.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(quantity!=0){
-                    quantity--;
-                    count.setText(String.valueOf(quantity));
-                }
-            }
-        });
-
-
         if (!product.getImageUrl().isEmpty())
             Picasso.get().load(product.getImageUrl()).into(image);
 
+        cancel.setOnClickListener(v -> dismiss());
 
-        cancel.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                dismiss();
-            }
+        add.setOnClickListener(v -> {
+            dialogListener.getQuantity(product, quantity);
+            dismiss();
         });
-
-        add.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                dialogListener.getQuantity(product , quantity);
-                dismiss();
-            }
-        });
-
 
         return view;
-    }
-
-    public interface DialogListener{
-        void getQuantity(Product product, int q);
     }
 
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-
-        try {
-            dialogListener = (DialogListener) context;
-        } catch (ClassCastException e) {
-            throw new ClassCastException(context.toString() + "must implement DialogListener");
-        }
+        dialogListener = (DialogListener) context;
     }
 
     @Override
@@ -151,5 +104,9 @@ public class PopupQuantityDialog extends DialogFragment {
 
         // Set window attributes
         window.setAttributes(windowParams);
+    }
+
+    public interface DialogListener {
+        void getQuantity(Product product, int q);
     }
 }
