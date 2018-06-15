bfc7f36b77c87d19b633ce2e5142612ff8b77f9e
DioExtreme <diodotexe@gmail.com>
2018-06-15 11:45:34 +0300

    Refactor CategoriesSearchPage class

--- front-end/PassBuy/app/src/main/java/thedreamteam/passbuy/CategoriesSearchPage.java
+++ front-end/PassBuy/app/src/main/java/thedreamteam/passbuy/CategoriesSearchPage.java
@@ -4,9 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
-import android.view.KeyEvent;
-import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.ImageButton;
 import android.widget.TextView;
@@ -17,25 +14,25 @@ import java.util.List;
 public class CategoriesSearchPage extends PortraitActivity {
 
 
-    private List<Category> categorynames = new ArrayList<>();
+    private List<Category> categoryNames = new ArrayList<>();
     private Basket basket = new Basket();
     private GsonWorker gson = new GsonWorker();
-    private CategoriesSearchPageAdapter mAdapter = new CategoriesSearchPageAdapter(this, categorynames, basket);
 
-    private TextView search_text;
-    private ImageButton search_button;
-    private ImageButton back_button;
-    private ImageButton home_screen;
+    private CategoriesSearchPageAdapter mAdapter = new CategoriesSearchPageAdapter(this, categoryNames, basket);
+    private TextView searchText;
+    private ImageButton searchButton;
+    private ImageButton backButton;
+    private ImageButton homeScreen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.categories_search_page);
 
-        search_text = findViewById(R.id.search_text);
-        search_button = findViewById(R.id.search_button);
-        back_button = findViewById(R.id.backButton);
-        home_screen = findViewById(R.id.homeButton);
+        searchText = findViewById(R.id.search_text);
+        searchButton = findViewById(R.id.search_button);
+        backButton = findViewById(R.id.backButton);
+        homeScreen = findViewById(R.id.homeButton);
 
 
         //get basket from previous activity
@@ -45,94 +42,66 @@ public class CategoriesSearchPage extends PortraitActivity {
 
         initRecyclerView();
 
-        Thread t = new Thread(() -> {
-            categorynames = gson.getCategories();
-            Log.i("Hey","Categories Obtained");
-            if(categorynames!= null){
-                mAdapter.replaceList(categorynames);
-                runOnUiThread(new Thread(() -> mAdapter.notifyDataSetChanged()));}
-            Log.i("runOnUi","Success");
+        new Thread(() -> {
+            categoryNames = gson.getCategories();
 
-        });
-        t.start();
-
-
-        search_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                Intent intent = new Intent(v.getContext(), SearchResults.class);
-
-                Bundle bundle = new Bundle();
-                bundle.putSerializable("basket", basket);
-                bundle.putCharSequence("search_text",search_text.getText().toString());
-                bundle.putInt("category_id", -1);
-                intent.putExtra("bundle",bundle);
-
-                startActivity(intent);
+            if (categoryNames != null) {
+                mAdapter.replaceList(categoryNames);
+                runOnUiThread(new Thread(() -> mAdapter.notifyDataSetChanged()));
             }
+        }).start();
+
+
+        searchButton.setOnClickListener(v -> {
+
+            Intent intent = new Intent(v.getContext(), SearchResults.class);
+
+            Bundle bundle1 = new Bundle();
+            bundle1.putSerializable("basket", basket);
+            bundle1.putCharSequence("search_text", searchText.getText().toString());
+            bundle1.putInt("category_id", -1);
+            intent.putExtra("bundle", bundle1);
+
+            startActivity(intent);
         });
 
-        home_screen.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent =  new Intent(v.getContext() , HomeScreen.class);
+        homeScreen.setOnClickListener(v -> {
+            Intent intent = new Intent(v.getContext(), HomeScreen.class);
 
-                Bundle bundle = new Bundle();
-                bundle.putSerializable("basket", basket);
-                intent.putExtra("bundle",bundle);
+            Bundle bundle12 = new Bundle();
+            bundle12.putSerializable("basket", basket);
+            intent.putExtra("bundle", bundle12);
 
-                startActivity(intent);
+            startActivity(intent);
+        });
+
+        searchText.setOnEditorActionListener((v, actionId, event) -> {
+            if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                searchButton.performClick();
+                return true;
             }
+            return false;
         });
 
-        search_text.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-            @Override
-            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                if (actionId == EditorInfo.IME_ACTION_SEARCH) {
-                    search_button.performClick();
-                    return true;
-                }
-                return false;
-            }
+        backButton.setOnClickListener(v -> {
+            Intent intent = new Intent(v.getContext(), HomeScreen.class);
+
+            Bundle bundle2 = new Bundle();
+            bundle2.putSerializable("basket", basket);
+            intent.putExtra("bundle", bundle2);
+
+            startActivity(intent);
         });
-
-        back_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent =  new Intent(v.getContext() , HomeScreen.class);
-
-                Bundle bundle = new Bundle();
-                bundle.putSerializable("basket", basket);
-                intent.putExtra("bundle",bundle);
-
-                startActivity(intent);
-            }
-        });
-
-
-
-
     }
 
-    public void onBackPressed(){
-
-        back_button.performClick();
-
+    public void onBackPressed() {
+        backButton.performClick();
     }
 
-
-    private void initImageBitmaps(){
-
-        initRecyclerView();
-
-    }
-
-    private void initRecyclerView(){
+    private void initRecyclerView() {
         RecyclerView recyclerView = findViewById(R.id.recycler_view33);
 
         recyclerView.setHasFixedSize(true);
-        //CategoriesSearchPageAdapter adapter = new CategoriesSearchPageAdapter(this, categorynames,basket);
         recyclerView.setAdapter(mAdapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
     }
