b76e061622905db60d96c730d4bf0a820dbdd681
DioExtreme <diodotexe@gmail.com>
2018-06-15 11:43:55 +0300

    Refactor Basket class

--- front-end/PassBuy/app/src/main/java/thedreamteam/passbuy/Basket.java
+++ front-end/PassBuy/app/src/main/java/thedreamteam/passbuy/Basket.java
@@ -1,8 +1,5 @@
 package thedreamteam.passbuy;
 
-import com.google.gson.annotations.Expose;
-import com.google.gson.annotations.SerializedName;
-
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
@@ -10,8 +7,8 @@ import java.util.List;
 public class Basket implements Serializable {
 
     private List<Product> products = new ArrayList<>();
-    private ArrayList<Integer> quantities = new ArrayList<>();
-    private List<Price> total_prices = new ArrayList<>();
+    private List<Integer> quantities = new ArrayList<>();
+    private List<StorePrice> totalPrices = new ArrayList<>();
 
     public List<Product> getProducts() {
         return products;
@@ -21,19 +18,19 @@ public class Basket implements Serializable {
         this.products = products;
     }
 
-    public ArrayList<Integer> getQuantities() {
+    public List<Integer> getQuantities() {
         return quantities;
     }
 
-    public void setQuantities(ArrayList<Integer> quantities) {
+    public void setQuantities(List<Integer> quantities) {
         this.quantities = quantities;
     }
 
-    public List<Price> getTotalprices() {
-        return total_prices;
+    public List<StorePrice> getTotalPrices() {
+        return totalPrices;
     }
 
-    public void setTotalprices(List<Price> total_prices) {
-        this.total_prices = total_prices;
+    public void setTotalPrices(List<StorePrice> totalPrices) {
+        this.totalPrices = totalPrices;
     }
 }
